bd1d6bf0a2e7a49a5bf256971de8039f46584022
Alexey Sobolev <sobolev-alexey@users.noreply.github.com>
2023-01-05 15:50:29 +0100

    feat(Products): products module

---
+++ backend/src/products/products.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
+import { Product, ProductSchema } from './products.schema';
+
+import { User, UserSchema } from '../users/user.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Product.name, schema: ProductSchema },
+    ]),
+  ],
+  providers: [ProductsService],
+  controllers: [ProductsController],
+})
+export class ProductsModule {}
