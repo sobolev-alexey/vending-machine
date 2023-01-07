936181fd7cfd3235c8b886235774d191f94a4adc
Alexey Sobolev <sobolev-alexey@users.noreply.github.com>
2023-01-07 19:23:42 +0100

    feat(Backend): create swagger api docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error'],
+  });
   const configService = app.get<ConfigService>(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(configService.get('PORT'));
+
+  const config = new DocumentBuilder()
+    .setTitle('Vending Machine backend')
+    .setDescription('')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(configService.get<string>('PORT') || 3001);
+  console.log(`Application is listening on: ${await app.getUrl()}`);
 }
 bootstrap();
