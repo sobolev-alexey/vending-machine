b84193333e2dc25492792ea4dc40acc75fdaf3cc
Alexey Sobolev <sobolev-alexey@users.noreply.github.com>
2023-01-09 01:51:28 +0100

    refactor(Frontend): cleanup

--- frontend/src/components/vending/index.tsx
+++ frontend/src/components/vending/index.tsx
@@ -1,17 +1,13 @@
 import Button from './Button';
 import Price from './Price';
 import Product from './Product';
-import ProductPlaceholder from './ProductPlaceholder';
 import ProductAnimation from './ProductAnimation';
 import Vending from './Vending';
-import data from './data';
 
 export {
   Button,
   Price,
   Product,
-  ProductPlaceholder,
   ProductAnimation,
   Vending,
-  data,
 };
