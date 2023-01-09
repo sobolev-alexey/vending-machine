49454ff3a31262d47ee5dfa6ded3d1b2be84446d
Alexey Sobolev <sobolev-alexey@users.noreply.github.com>
2023-01-09 01:56:10 +0100

    feat(Frontend): update imports

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -3,6 +3,9 @@ import CustomAuthHeader from './common/CustomAuthHeader';
 import ErrorFallback from './common/ErrorFallback';
 import Logout from './common/Logout';
 import Vending from './vending/Vending';
+import ModeSelector from './seller/ModeSelector';
+import AddProducts from './seller/AddProducts';
+import ManageProducts from './seller/ManageProducts';
 
 export {
   Loading,
@@ -10,4 +13,7 @@ export {
   ErrorFallback,
   CustomAuthHeader,
   Vending,
+  ModeSelector,
+  AddProducts,
+  ManageProducts
 };
